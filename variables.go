c532f1de7b6f86354c5a676e4be7d43158309f1f
System Administrator <khanhnnvn@gmail.com>
2016-04-28 01:33:34 +0700

    Variables

---
+++ variables.go
@@ -0,0 +1,17 @@
+package main
+import "fmt"
+func main(){
+  var a string = "Course Bridge"
+  fmt.Println(a)
+
+  var b, c int = 10, 20
+  fmt.Println(b, c)
+  var d = true
+  fmt.Println(d)
+  var e int
+  fmt.Println(e)
+  f := "KhanhNN"
+  fmt.Println(f)
+  g := 10
+  fmt.Println(g)
+}
