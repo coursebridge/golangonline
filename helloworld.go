aa7eee3896338c8db8df7595d62ed2228b2f61cd
System Administrator <khanhnnvn@gmail.com>
2016-04-28 01:19:13 +0700

    comment cua Khanh

---
+++ helloworld.go
@@ -0,0 +1,7 @@
+package main
+import "fmt"
+func main() {
+  fmt.Println("--------Course Bridge - Golang Online------")
+  fmt.Println("Hello Python Viet Nam")
+  fmt.Println("Hello Python Viet Namd ")
+}
