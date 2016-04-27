467806f82d9e213cec492fd2899710a6b5e89ac6
System Administrator <khanhnnvn@gmail.com>
2016-04-28 01:26:56 +0700

    Values

---
+++ values.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+func main(){
+  fmt.Println("Chao mung cac ban " + "den voi khoa hoc Golang")
+  fmt.Println("Gia tri cua 100 + 200: ", 100+200)
+  fmt.Println("Gia tri cua 7.0/3.0: ", 7.0/3.0)
+
+  fmt.Println(true && false)
+  fmt.Println(true || false)
+  fmt.Println(!true)
+}
